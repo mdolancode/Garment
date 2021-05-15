0044b569f5b8102d8cc2368ee3bfba0991d5c72d
Matt Dolan <mdolan.code@gmail.com>
2021-05-14 22:24:26 -0700

    Set IBOutlet's and IBAction's for alphaButton, creationTimeButton and addBarButtonItem.

---
+++ Matthew-Dolan-Lululemon-iOS-Assessment/ListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListViewController.swift
+//  Matthew-Dolan-Lululemon-iOS-Assessment
+//
+//  Created by Matt Dolan External macOS on 2021-05-14.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var addBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var alphaButton: UIButton!
+    @IBOutlet weak var creationTimeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addBarButtonItemPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func alphaButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func creationTimeButtonPressed(_ sender: UIButton) {
+    }
+}
+
