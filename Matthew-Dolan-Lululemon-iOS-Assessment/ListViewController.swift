3666e6a8df125e3ce196833cfeee0b4711fb19f9
Matt Dolan <mdolan.code@gmail.com>
2021-05-15 07:44:25 -0700

    Add tableView and tableViewCell. Set constraints and IBOutlet for tableView. Add border for top and bottom of tableView. Mark sections.

--- Matthew-Dolan-Lululemon-iOS-Assessment/ListViewController.swift
+++ Matthew-Dolan-Lululemon-iOS-Assessment/ListViewController.swift
@@ -13,11 +13,13 @@ class ListViewController: UIViewController {
     @IBOutlet weak var addBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var alphaButton: UIButton!
     @IBOutlet weak var creationTimeButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationBarUI()
         buttonUI()
+        addTopAndBottomBorder()
     }
     
     @IBAction func addBarButtonItemPressed(_ sender: UIBarButtonItem) {
@@ -29,10 +31,14 @@ class ListViewController: UIViewController {
     @IBAction func creationTimeButtonPressed(_ sender: UIButton) {
     }
     
+    //MARK: - NavigationBarUI
+    
     func navigationBarUI() {
         navigationBar.barTintColor = .white
         addBarButtonItem.tintColor = .black
     }
+    
+    //MARK: - ButtonUI
     
     func buttonUI() {
         // alphaButton
@@ -43,14 +49,27 @@ class ListViewController: UIViewController {
         creationTimeButton.backgroundColor = .black
         creationTimeButton.roundCorners(corners: [.topRight, .bottomRight], radius: 10)
     }
+    
+    //MARK: - TableView Border
+    
+    func addTopAndBottomBorder() {
+        let thickness: CGFloat = 2.0
+        let topBorder = CALayer()
+        let bottomBorder = CALayer()
+        topBorder.frame = CGRect(x: 0.0, y: 0.0, width: self.tableView.frame.size.width, height: thickness)
+        topBorder.backgroundColor = UIColor.black.cgColor
+        bottomBorder.frame = CGRect(x: 0.0, y: self.tableView.frame.size.height - thickness, width: self.tableView.frame.size.width, height: thickness)
+        bottomBorder.backgroundColor = UIColor.black.cgColor
+        tableView.layer.addSublayer(topBorder)
+        tableView.layer.addSublayer(bottomBorder)
+    }
 }
 
-//MARK: - UIButton
+    //MARK: - UIButton
+
 extension UIButton {
     func roundCorners(corners: UIRectCorner, radius: Int = 8) {
-        let maskPath1 = UIBezierPath(roundedRect: bounds,
-                                     byRoundingCorners: corners,
-                                     cornerRadii: CGSize(width: radius, height: radius))
+        let maskPath1 = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         let maskLayer1 = CAShapeLayer()
         maskLayer1.frame = bounds
         maskLayer1.path = maskPath1.cgPath
