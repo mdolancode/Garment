6460af5ac776dd082c6ebd8c7dfd4f879c6483a9
Matt Dolan <mdolan.code@gmail.com>
2021-05-16 15:12:02 -0700

    Create struct for GarmentModel and add garmentArray.

---
+++ Matthew-Dolan-Lululemon-iOS-Assessment/Data Model/GarmentModel.swift
@@ -0,0 +1,12 @@
+//
+//  GarmentModel.swift
+//  Matthew-Dolan-Lululemon-iOS-Assessment
+//
+//  Created by Matt Dolan External macOS on 2021-05-16.
+//
+
+import Foundation
+
+struct GarmentModel {
+    var garmentArray: [String]?
+}
