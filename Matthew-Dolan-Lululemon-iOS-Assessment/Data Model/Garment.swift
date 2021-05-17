aa305b034d78f5f14e2615f7c3fe64d2b7b19100
Matt Dolan <mdolan.code@gmail.com>
2021-05-17 09:32:21 -0700

    Create swift file and Garment Struct.

---
+++ Matthew-Dolan-Lululemon-iOS-Assessment/Data Model/Garment.swift
@@ -0,0 +1,12 @@
+//
+//  Garment.swift
+//  Matthew-Dolan-Lululemon-iOS-Assessment
+//
+//  Created by Matt Dolan External macOS on 2021-05-17.
+//
+
+import UIKit
+
+struct Garment {
+    var garmentName: String
+}
