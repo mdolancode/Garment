3368eda16054f7747543e7e1d84a680f0d1ada65
Matt Dolan <mdolan.code@gmail.com>
2021-05-18 11:53:10 -0700

    Add dateCreated in GarmentData Object.

--- Matthew-Dolan-lululemon-iOS-Assessment/Data Model/GarmentData.swift
+++ Matthew-Dolan-lululemon-iOS-Assessment/Data Model/GarmentData.swift
@@ -10,13 +10,16 @@ import RealmSwift
 
 protocol AddGarmentDelegate {
     func addGarment(_ garment: GarmentData)
+    func addDateCreated(_ dateCreated: GarmentData)
 }
 
 class GarmentData: Object {
     @objc dynamic var garmentName: String = ""
+    @objc dynamic var dateCreated: Date?
     
-    convenience init(garmentName: String) {
+    convenience init(garmentName: String, dateCreated: Date) {
         self.init()
         self.garmentName = garmentName
+        self.dateCreated = dateCreated
     }
 }
