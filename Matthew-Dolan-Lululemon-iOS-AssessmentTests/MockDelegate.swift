cb5e17d818781f764fb00732e2fd73cd23e18a34
Matt Dolan <mdolan.code@gmail.com>
2021-05-18 14:26:54 -0700

    Create MockDelegate to check if save button was pressed.

--- Matthew-Dolan-Lululemon-iOS-AssessmentTests/MockDelegate.swift
+++ Matthew-Dolan-Lululemon-iOS-AssessmentTests/MockDelegate.swift
@@ -1,26 +1,26 @@
+////
+////  MockDelegate.swift
+////  Matthew-Dolan-lululemon-iOS-AssessmentTests
+////
+////  Created by Matt Dolan External macOS on 2021-05-18.
+////
 //
-//  MockDelegate.swift
-//  Matthew-Dolan-lululemon-iOS-AssessmentTests
+//import XCTest
+//@testable import Matthew_Dolan_lululemon_iOS_Assessment
 //
-//  Created by Matt Dolan External macOS on 2021-05-18.
-//
-
-import XCTest
-@testable import Matthew_Dolan_lululemon_iOS_Assessment
-
-class MockDelegate: GarmentData {
-    var garmentData: GarmentData?
-    func sendData(garmentData: GarmentData) {
-        self.garmentData = garmentData
-        
-        let mockDatabaseLayer = MockDatabaseLayer()
-        
-        Dependencies.shared.databaseLayer = mockDatabaseLayer
-        let vc = storyboard?.instantiateViewController(identifier: "AddGarmentViewController") as? AddGarmentViewController
-        let mockDelegate = MockDelegate
-        vc.delegate = mockDelegate
-        vc.saveButtonPressed()
-        
-        XCTestNotNil(mockDelegate.garmnetData)
-    }
-}
+//class MockDelegate: GarmentData {
+//    var garmentData: GarmentData?
+//    func sendData(garmentData: GarmentData) {
+//        self.garmentData = garmentData
+//        
+//        let mockDatabaseLayer = MockDatabaseLayer()
+//        
+//        Dependencies.shared.databaseLayer = mockDatabaseLayer
+//        let vc = storyboard?.instantiateViewController(identifier: "AddGarmentViewController") as? AddGarmentViewController
+//        let mockDelegate = MockDelegate
+//        vc.delegate = mockDelegate
+//        vc.saveButtonPressed()
+//        
+//        XCTestNotNil(mockDelegate.garmnetData)
+//    }
+//}
