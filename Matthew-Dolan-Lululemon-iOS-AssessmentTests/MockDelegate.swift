e697e63ec7e6f01105d2eb35e022701cbfb2e827
Matt Dolan <mdolan.code@gmail.com>
2021-05-19 00:47:25 -0700

    Create MockDelegate for PersistenceEventDelegate.

--- Matthew-Dolan-Lululemon-iOS-AssessmentTests/MockDelegate.swift
+++ Matthew-Dolan-Lululemon-iOS-AssessmentTests/MockDelegate.swift
@@ -1,26 +1,16 @@
-////
-////  MockDelegate.swift
-////  Matthew-Dolan-lululemon-iOS-AssessmentTests
-////
-////  Created by Matt Dolan External macOS on 2021-05-18.
-////
 //
-//import XCTest
-//@testable import Matthew_Dolan_lululemon_iOS_Assessment
+//  MockDelegate.swift
+//  Matthew-Dolan-lululemon-iOS-AssessmentTests
 //
-//class MockDelegate: GarmentData {
-//    var garmentData: GarmentData?
-//    func sendData(garmentData: GarmentData) {
-//        self.garmentData = garmentData
-//        
-//        let mockDatabaseLayer = MockDatabaseLayer()
-//        
-//        Dependencies.shared.databaseLayer = mockDatabaseLayer
-//        let vc = storyboard?.instantiateViewController(identifier: "AddGarmentViewController") as? AddGarmentViewController
-//        let mockDelegate = MockDelegate
-//        vc.delegate = mockDelegate
-//        vc.saveButtonPressed()
-//        
-//        XCTestNotNil(mockDelegate.garmnetData)
-//    }
-//}
+//  Created by Matt Dolan External macOS on 2021-05-18.
+//
+
+import XCTest
+@testable import Matthew_Dolan_lululemon_iOS_Assessment
+
+class MockDelegate: PersistenceEventDelegate {
+    var didSaveDataCalled = false
+    func didSaveData() {
+        didSaveDataCalled = true
+    }
+}
