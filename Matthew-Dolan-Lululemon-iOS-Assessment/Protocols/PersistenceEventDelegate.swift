00b803ddc82130555b0c73521887305b63e9cf67
Matt Dolan <mdolan.code@gmail.com>
2021-05-19 00:34:20 -0700

    Create PersistenceEventDelegate protocol.

---
+++ Matthew-Dolan-Lululemon-iOS-Assessment/Protocols/PersistenceEventDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  PersistenceEventDelegate.swift
+//  Matthew-Dolan-lululemon-iOS-Assessment
+//
+//  Created by Matt Dolan External macOS on 2021-05-18.
+//
+
+import Foundation
+
+protocol PersistenceEventDelegate: AnyObject {
+    func didSaveData()
+}
