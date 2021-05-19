9d316bf8833f1761d1c9a4c1e551b4afa6df7640
Matt Dolan <mdolan.code@gmail.com>
2021-05-19 00:45:31 -0700

    Refactor dependencies.

--- Matthew-Dolan-lululemon-iOS-Assessment/Dependencies/Dependencies.swift
+++ Matthew-Dolan-lululemon-iOS-Assessment/Dependencies/Dependencies.swift
@@ -5,32 +5,15 @@
 //  Created by Matt Dolan External macOS on 2021-05-18.
 //
 
-import UIKit
+import Foundation
 
-class Dependencies: HasDatabase, HasGarmentData, HasApplication {
-    let database: DatabaseLayer
-    let garmentData: GarmentData
-    let application: UIApplication
+class Dependencies {
+    
+    static let shared = Dependencies()
+    
+    var database: DatabaseLayer
 
-    init(database: DatabaseLayer? = nil,
-         garmentData: GarmentData? = nil,
-         application: UIApplication?) {
-        let garment = garmentData ?? GarmentData()
-        self.database = database ?? DatabaseLayer()
-        self.garmentData = garment
-        self.application = application ?? UIApplication.shared
+    private init(database: DatabaseLayer = DatabaseLayer()) {
+        self.database = database
     }
 }
-
-protocol HasDatabase {
-    var database: DatabaseLayer { get }
-}
-
-protocol HasGarmentData {
-    var garmentData: GarmentData { get }
-}
-
-protocol HasApplication {
-    var application: UIApplication { get }
-}
-
